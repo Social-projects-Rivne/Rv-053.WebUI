8e9b345e130d5c3433b7c346dc86aa7c8d938693
Hryhorii Kucherchuk <dragonx@meta.ua>
2020-01-29 13:56:36 +0200

    adaptive header

--- client/src/shared/components/Header/Header.js
+++ client/src/shared/components/Header/Header.js
@@ -1,45 +1,78 @@
 import React, { useContext } from 'react';
-import classes from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../../context/auth-context';
+
+import './Header.css';
 
 const Header = () => {
   const auth = useContext(AuthContext);
   return (
-    <div className={classes.header}>
-      <div className={classes.container}>
-        <div className="row">
-          <div className="col-7">
-            <div className={classes.logo}>
-              <span>E</span>eeeevent
-            </div>
-          </div>
-          <div className={'col-5 text-nowrap ' + classes.navigation}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/events">All events</NavLink>
-            <NavLink to="/">Calendar</NavLink>
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#"> <span>E</span>eeeevent</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+            <NavLink className="nav-link" to="/">Home</NavLink> 
+        </li>
+        <li className="nav-item active">
+            <NavLink className="nav-link" to="/events">All events</NavLink>
+        </li>
+        <li className="nav-item active">
+           <NavLink className="nav-link" to="/">Calendar</NavLink>
+        </li>
+        <li className="nav-item active">
             {!auth.isLoggedIn && (
-              <NavLink to="/auth">
-                <button className="btn btn-outline-success btn-block">
-                  Sign In
-                </button>
-              </NavLink>
+                <NavLink className="nav-link" to="/auth">
+                  <button className="btn btn-outline-success btn-block">
+                    Sign In
+                  </button>
+                </NavLink>
             )}
-            {auth.isLoggedIn && (
-              <NavLink to="/">
-                <button
-                  className="btn btn-outline-success btn-block"
-                  onClick={auth.logout}
-                >
-                  Logout
-                </button>
-              </NavLink>
+        </li>
+        <li className="nav-item">
+           {auth.isLoggedIn && (
+                <NavLink className="nav-link" to="/">
+                  <button
+                    className="btn btn-outline-success btn-block"
+                    onClick={auth.logout}
+                  >
+                    Logout
+                  </button>
+                </NavLink>
             )}
-          </div>
+        </li>
+          </ul>
         </div>
-      </div>
-    </div>
+    </nav>
   );
 };
+
+// <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//   <a className="navbar-brand" href="#">Navbar</a>
+//   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//     <span className="navbar-toggler-icon"></span>
+//   </button>
+//   <div className="collapse navbar-collapse" id="navbarNav">
+//     <ul className="navbar-nav">
+//       <li className="nav-item active">
+//         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+//       </li>
+//       <li className="nav-item">
+//         <a className="nav-link" href="#">Features</a>
+//       </li>
+//       <li className="nav-item">
+//         <a className="nav-link" href="#">Pricing</a>
+//       </li>
+//       <li className="nav-item">
+//         <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+//       </li>
+//     </ul>
+//   </div>
+// </nav>
+
+
 
 export default Header;
