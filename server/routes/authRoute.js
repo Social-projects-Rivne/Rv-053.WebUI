600e872b17440036e95e5fb7b31a93dbbc6a92be
Vlad Ovchynnyk <ovchnkvlad@gmail.com>
2020-02-03 20:14:23 +0200

    replace passport.js to config folder

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const passport = require("passport");
-const passportConf = require("../passport");
+const passportConf = require("../config/passport");
 const AuthController = require("../controllers/authController");
-const passportSingIn = function(req, res, next) {
-  passport.authenticate("local", function(err, user, info) {
+const passportSingIn = function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
     if (err) {
       return next(err);
     }
     if (!user) {
-      return res.status(401).json({ message: info.message });
+      return res.status(401).json({
+        message: info.message
+      });
     }
     req.user = user;
     next();
