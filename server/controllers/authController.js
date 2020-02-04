44c33dbbb8b57e4b7d1a56cd3fd1ebd49550c86c
Lena Pyvnytska <lpyvnytska@gmail.com>
2020-02-04 16:50:36 +0200

    Save refresh token in cookies

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 //!!!!!!!!!
 //Model User:
-const User = require("../models").users;
-const Token = require("../models").token;
+const User = require('../models').users;
+const Token = require('../models').token;
 //get value from config/default.json
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRE_IN = process.env.JWT_EXPIRE_IN;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 const JWT_REFRESH_EXPIRE_IN = process.env.JWT_REFRESH_EXPIRE_IN;
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
-const tokenService = require("../services/tokenService");
+const tokenService = require('../services/tokenService');
 
 const updateTokens = user => {
   const accessToken = tokenService.generateAccessToken(user);
@@ -28,18 +28,6 @@ const updateTokens = user => {
       expiresIn: decoded_access.exp
     }));
 };
-// const tokenService = new TokenService();
-
-// singToken = user => {
-//   return JWT.sign(
-//     {
-//       sub: user.id,
-//       iat: new Date().getTime(), //current time
-//       exp: new Date().getTime() + JWT_EXPIRE_IN //current time + JWT_EXPIRE_IN
-//     },
-//     JWT_SECRET
-//   );
-// };
 
 exports.signUp = async (req, res) => {
   try {
@@ -47,7 +35,7 @@ exports.signUp = async (req, res) => {
     const foundUser = await User.findOne({ where: { email } });
     //if user exist return res
     if (foundUser) {
-      return res.status(200).json({ error: "Email is already in use" });
+      return res.status(200).json({ error: 'Email is already in use' });
     }
     //else create new user into DB and generate token
     const hashPassword = await bcrypt.hash(password, saltRounds);
@@ -55,11 +43,11 @@ exports.signUp = async (req, res) => {
     await User.create({
       email: email,
       password: hashPassword,
-      sex: req.body.sex || "Unknown",
-      first_name: req.body.first_name || "",
-      last_name: req.body.last_name || "",
-      phone: req.body.phone || "",
-      role: "User",
+      sex: req.body.sex || 'Unknown',
+      first_name: req.body.first_name || '',
+      last_name: req.body.last_name || '',
+      phone: req.body.phone || '',
+      role: 'User',
       status_id: 1
     });
 
@@ -74,24 +62,29 @@ exports.signIn = async (req, res) => {
   //console.log("start authController.signIn ");
   updateTokens(req.user).then(tokens => {
     //console.log(tokens);
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true
+    });
     res.status(200).json({
       token: tokens.token,
-      refreshToken: tokens.refreshToken,
       expiresIn: tokens.expiresIn
     });
   });
 };
 
 exports.refreshTokens = async (req, res) => {
-  const { refreshToken } = req.body;
+  //const { refreshToken } = req.body;
+  console.log('AAAAAAAAAAAAAAAAA');
+  const { refreshToken } = req.cookies;
+  console.log(refreshToken);
   let payload;
   try {
     payload = await jwt.verify(refreshToken, JWT_REFRESH_SECRET);
   } catch (e) {
     if (e instanceof jwt.TokenExpiredError) {
-      res.status(400).json({ message: "Token expired" });
+      res.status(400).json({ message: 'Token expired' });
     } else if (e instanceof jwt.JsonWebTokenError) {
-      res.status(400).json({ message: "Invalid token" });
+      res.status(400).json({ message: 'Invalid token' });
     }
     return;
   }
@@ -100,7 +93,7 @@ exports.refreshTokens = async (req, res) => {
   await Token.findOne({ where: { id: payload.id } })
     .then(token => {
       if (token === null) {
-        throw new Error("Invalid token!");
+        throw new Error('Invalid token!');
       }
       const user = User.findOne({ where: { id: token.user_id } });
       if (!user) {
@@ -108,7 +101,16 @@ exports.refreshTokens = async (req, res) => {
       }
       return updateTokens(user);
     })
-    .then(tokens => res.json(tokens))
+    .then(tokens => {
+      console.log(tokens);
+      res.cookie('refreshToken', tokens.refreshToken, {
+        httpOnly: true
+      });
+      res.status(200).json({
+        token: tokens.token,
+        expiresIn: tokens.expiresIn
+      });
+    })
     .catch(err => res.status(400).json({ message: err.message }));
 };
 
@@ -124,7 +126,7 @@ exports.signOut = async (req, res) => {
     });
     const token = jwt.sign(
       {
-        sub: "Logout",
+        sub: 'Logout',
         iat: new Date().getTime(), //current time
         exp: new Date().getTime() //current time
       },
