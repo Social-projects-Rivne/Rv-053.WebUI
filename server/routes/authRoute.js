ccc3c69c817ba4ff282e8bffe9790b4c6d905c55
Lena Pyvnytska <lpyvnytska@gmail.com>
2020-02-05 13:27:59 +0200

    update logout

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const passport = require("passport");
-const passportConf = require("../passport");
-const AuthController = require("../controllers/authController");
+const express = require('express');
+const passport = require('passport');
+const passportConf = require('../passport');
+const AuthController = require('../controllers/authController');
 const passportSingIn = function(req, res, next) {
-  passport.authenticate("local", function(err, user, info) {
+  passport.authenticate('local', function(err, user, info) {
     if (err) {
       return next(err);
     }
@@ -15,23 +15,23 @@ const passportSingIn = function(req, res, next) {
   })(req, res, next);
 };
 
-const passportGoogle = passport.authenticate("google", {
-  scope: ["profile", "email"],
+const passportGoogle = passport.authenticate('google', {
+  scope: ['profile', 'email'],
   session: false
 });
 const router = express.Router();
-const auth = require("../middlewares/authorization");
+const auth = require('../middlewares/authorization');
 
-router.post("/register", AuthController.signUp);
+router.post('/register', AuthController.signUp);
 
-router.post("/login", passportSingIn, AuthController.signIn);
+router.post('/login', passportSingIn, AuthController.signIn);
 
-router.post("/logout", AuthController.signOut);
+router.post('/logout', AuthController.signOut);
 
-router.post("/check", AuthController.checkAuth);
-router.post("/refresh", AuthController.refreshTokens);
+//router.post("/check", AuthController.checkAuth);
+router.post('/refresh', AuthController.refreshTokens);
 
-router.get("/google", passportGoogle);
-router.get("/google/redirect", passportGoogle, AuthController.signIn);
+router.get('/google', passportGoogle);
+router.get('/google/redirect', passportGoogle, AuthController.signIn);
 
 module.exports = router;
