393e241ccfadb591083a3550c12284b1988806e1
Vlad Ovchynnyk <ovchnkvlad@gmail.com>
2020-02-06 12:49:17 +0200

    add methods to create new event

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,7 +1,88 @@
 const Event = require("../models").event;
+const User = require("../models").users;
 const Sequelize = require("sequelize");
 const sequelize = require("../models").sequelize;
 const Op = Sequelize.Op;
+
+// Get all events
+exports.getAllEvent = async (req, res) => {
+  await Event.findAndCountAll({
+    raw: true
+  }).then(events => {
+    res.status(200).json(events);
+  }).catch(err => {
+    res.status(404).send({
+      message: err.message || "Not found"
+    });
+  });
+}
+
+// Get event by ID
+exports.getEventByID = async (req, res) => {
+  // Get event ID from req.params
+  const {
+    id
+  } = req.params;
+  await Event.findOne({
+    where: {
+      id: id
+    },
+    include: [{
+      // Add info about the user to res
+      model: User,
+      attributes: ['first_name', 'last_name', 'avatar']
+    }]
+  }).then(event => {
+    if (event === null) {
+      res.status(404).send({
+        message: 'Event not found'
+      });
+    }
+    res.status(200).json(event);
+  }).catch(err => {
+    res.status(404).send({
+      message: err.message || "Not found"
+    });
+  });
+}
+
+// Add new event to DB
+exports.createEvent = async (req, res) => {
+  // Get event ID from req.params
+  const {
+    name,
+    description,
+    location,
+    datetime,
+    duration,
+    max_participants,
+    min_age,
+    cover,
+    price
+  } = req.body;
+  const ownerID = req.userID;
+
+  await Event.create({
+    name,
+    owner_id: ownerID,
+    description,
+    location,
+    datetime,
+    duration,
+    max_participants,
+    min_age,
+    cover,
+    price
+  }).then(event => {
+    res.status(200).send({
+      message: 'Event was create successful'
+    });
+  }).catch(err => {
+    res.status(404).send({
+      message: err.message || "Something wrong"
+    });
+  });
+}
 
 exports.searchEvent = async (req, res) => {
   //If there is query 'q=some text'
--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -2,6 +2,10 @@ const express = require("express");
 const eventController = require("../controllers/eventController");
 const router = express.Router();
 
+router.get('/', eventController.getAllEvent);
+router.post('/create', eventController.createEvent);
+router.get('/:id', eventController.getEventByID);
+
 router.get("/?", eventController.searchEvent);
 
 module.exports = router;
