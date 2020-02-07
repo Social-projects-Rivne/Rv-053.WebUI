fc7b46634d846a5429fe62775e987dc3f3045c67
Vlad Ovchynnyk <ovchnkvlad@gmail.com>
2020-02-07 22:12:57 +0200

    add validation for create event fields

--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -2,10 +2,16 @@ const express = require("express");
 const eventController = require("../controllers/eventController");
 const router = express.Router();
 
+const {
+  createEventValidation,
+  validate
+} = require('../middlewares/validator');
+
+
 // Search event by name or description/get all event 
 router.get('/', eventController.searchEvent);
 // Create new event
-router.post('/', eventController.createEvent);
+router.post('/', createEventValidation(), validate, eventController.createEvent);
 // Get info about current event by id
 router.get('/:id', eventController.getEventByID);
 
