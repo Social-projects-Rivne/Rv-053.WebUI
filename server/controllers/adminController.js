3d7d31eb2339eb77f6af31f6ad83a263630c348a
Lena Pyvnytska <lpyvnytska@gmail.com>
2020-02-12 17:57:05 +0200

    get list of all users by admin

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,5 +1,24 @@
 const User = require('../models').users;
 
 exports.getAllUsers = async (req, res) => {
-  res.status(200).json({ message: 'GetAllUsers' });
+  const limit = req.query.limit || null;
+  const offset = req.query.offset || 0;
+  if (req.query.q) {
+  } else {
+    await User.findAndCountAll({
+      attributes: { exclude: ['password'] },
+      raw: true,
+      offset,
+      limit,
+      order: [['id', 'DESC']]
+    })
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(err => {
+        res.status(404).send({
+          message: err.message || 'Not found'
+        });
+      });
+  }
 };
