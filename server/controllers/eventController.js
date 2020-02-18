2b4b76646fcc4f845d8efb602495bed28d2dd286
Vlad Ovchynnyk <ovchnkvlad@gmail.com>
2020-02-18 22:23:25 +0200

    posibility to edit event by user and admin

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -80,6 +80,57 @@ exports.updateEvent = async (req, res) => {
     price
   } = req.body;
 
+  await Event.findOne({
+    where: {
+      id: req.userId
+    }
+  }).then(event => {
+    const editEvent = {};
+    if (name !== event.name && name !== null) {
+      editEvent.name = name;
+    }
+    if (description !== event.description && description !== null) {
+      editEvent.description = description;
+    }
+    if (location !== event.location && location !== null) {
+      editEvent.location = location;
+    }
+    if (datetime !== event.datetime && datetime !== null) {
+      editEvent.datetime = datetime;
+    }
+    if (duration !== event.duration && duration !== null) {
+      editEvent.duration = duration;
+    }
+    if (max_participants !== event.max_participants && max_participants !== null) {
+      editEvent.max_participants = max_participants;
+    }
+    if (min_age !== event.min_age && min_age !== null) {
+      editEvent.min_age = min_age;
+    }
+    if (cover !== event.cover && cover !== null) {
+      editEvent.cover = cover;
+    }
+    if (price !== event.price && price !== null) {
+      editEvent.price = price;
+    }
+    if (req.userId === event.owner_id || req.role === 'Admin') {
+      Event.update(editEvent, {
+        where: {
+          id: req.userId,
+          owner_id: req.userId
+        }
+      })
+        .then(() => {
+          res.status(200).json({ status: 'Event was update successful' });
+        })
+        .catch(err => {
+          res.status(404).json({
+            message: err.message || 'Event not found'
+          });
+        });
+    }
+  });
+
   await Event.update(
     { name, description, location, datetime, duration, max_participants, min_age, cover, price },
     {
@@ -105,14 +156,11 @@ exports.updateEvent = async (req, res) => {
 };
 
 exports.deleteEvent = async (req, res) => {
-  await Event.findOne(
-    { status: 'Deleted' },
-    {
-      where: {
-        id: req.params.id
-      }
+  await Event.findOne({
+    where: {
+      id: req.params.id
     }
-  )
+  })
     .then(event => {
       if (event === null) {
         res.status(404).json({
