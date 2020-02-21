2b5a04c712829f4041cabc850326fe6f20bb6d93
Hryhorii Kucherchuk <dragonx@meta.ua>
2020-02-21 14:21:29 +0200

    added notification of success update

--- client/src/users/pages/UserProfile.js
+++ client/src/users/pages/UserProfile.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 
 import UserInfo from './../components/UserInfo';
 import CategoriesList from './../components/CategoriesList';
 import FollowedEventList from './../components/ProfileEvents/FollowedEventList';
 import CreatedEventList from './../components/ProfileEvents/CreatedEventList';
+import SuccessNote from './../../shared/components/UI/SuccessNote';
 import './UserProfile.css';
-import { useLocation, useHistory } from 'react-router-dom';
 
 const UserProfile = () => {
   const history = useHistory();
   const location = useLocation();
   const [showSuccessNote, setShowSuccessNote] = useState(location.state?.showUpdateNotification);
-
+  const closeNote = () => {
+    setShowSuccessNote(false);
+  };
   useEffect(() => {
     // setShowSuccessNote({ showUpdateNotification: false });
     history.replace(location.pathname, { showUpdateNotification: false });
   }, []);
   return (
     <div className="profile-container">
+      {' '}
+      {showSuccessNote ? <SuccessNote click={closeNote} /> : null}
       <section className="profile_inner">
-        {showSuccessNote ? 'WOHOO' : null}
         <UserInfo />
         <CategoriesList />
         <div className="profile-followed_events">
