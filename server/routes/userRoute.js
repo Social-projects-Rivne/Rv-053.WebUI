2372473bb7acbc68ca1ec18fd6a4829d1c32c56b
Yurii Shevchuk <vanuchoff@gmail.com>
2020-02-23 22:57:44 +0200

    Refactor code

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -7,15 +7,16 @@ const authAdmin = require('../middlewares/adminAuthorization');
 
 route.get('/current', auth, userController.getCurrent);
 route.put('/current', auth, userController.updateProfile);
-route.get('/:id', auth, userController.getById);
 route.get('/events', auth, userController.getEvents);
 route.get('/followed-events', auth, userController.getFollowedEvents);
 route.delete('/unfollow-event/:id', auth, userController.unfollowFromEvent);
 route.get('/categories', auth, userController.getCategories);
 route.get('/followed-categories', auth, userController.getFollowedCategories);
+route.put('/role-admin/:id', authAdmin, userController.setRoleToAdmin);
 route.put('/role-moderator/:id', authAdmin, userController.setRoleToModerator);
 route.put('/role-user/:id', authAdmin, userController.setRoleToUser);
 route.post('/ban/:id', authAdmin, userController.ban);
 route.delete('/unban/:id', authAdmin, userController.unban);
+route.get('/:id', auth, userController.getById);
 
 module.exports = route;
