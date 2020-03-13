f8844b7ef41d8e4e8bfff88c7612538d523d826c
Yurii Shevchuk <vanuchoff@gmail.com>
2020-03-13 23:57:34 +0200

    Implement the possibility to send email for reset password

--- client/src/users/components/Signin.js
+++ client/src/users/components/Signin.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import { VAL_EMAIL, VAL_REQUIRED } from '../../shared/utilities/validation';
+import { NavLink } from 'react-router-dom';
 
 const Signin = props => {
   return (
@@ -24,6 +25,9 @@ const Signin = props => {
           onInput={props.inputHandler}
           errorMessage="Password is required"
         />
+        <NavLink to={'/password_reset'} className="forgot-link d-flex align  justify-content-end">
+          Forgot password?
+        </NavLink>
         <button className="my__button float-right mt-4" type="submit">
           Sign In
         </button>
