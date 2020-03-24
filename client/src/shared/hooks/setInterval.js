90d18abda68ee45c6b04fa421075ebb9721ee935
Mykhailo Shevtsov <shevtsov.misha19@gmail.com>
2020-03-24 12:43:17 +0200

    Deleted comments

--- client/src/shared/hooks/setInterval.js
+++ client/src/shared/hooks/setInterval.js
@@ -3,12 +3,10 @@ import React, { useState, useEffect, useRef } from 'react';
 function useInterval(callback, delay) {
   const savedCallback = useRef();
 
-  // Remember the latest callback.
   useEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
 
-  // Set up the interval.
   useEffect(() => {
     function tick() {
       savedCallback.current();
